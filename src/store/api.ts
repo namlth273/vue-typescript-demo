a461f37859814089687c25460247e1a33325ef7e
Hai nam LE TRINH <hai-nam.letrinh@bollore.com>
2019-02-19 18:39:10 +0700

    deploy azure

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const baseApi = axios.create({
-    baseURL: "/api"
-    // baseURL: "https://namlth-demo-api.azurewebsites.net/api"
+    // baseURL: "/api"
+    baseURL: "https://namlth-demo-api.azurewebsites.net/api"
 });
